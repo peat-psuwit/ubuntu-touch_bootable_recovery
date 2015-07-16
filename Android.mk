5ba9f2193f8ea77a0dd29303308abd8d422c666c
Ratchanan Srirattanamet <peathot@hotmail.com>
2015-07-16 19:43:06 +0700

    Create make.ext4fs, a small wrapper around mke2fs.
    system-image-upgrader uses make.ext4fs to format (real) system
    partition. However, this program seems to not be in normal Android
    tree. This commit implements this program as a small shell script
    wrapper to mke2fs, which is available in recovery.

--- Android.mk
+++ Android.mk
@@ -206,6 +206,14 @@ LOCAL_SRC_FILES := archive-master.tar.xz.asc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := make.ext4fs
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+LOCAL_SRC_FILES := make.ext4fs
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := verifier_test.c verifier.c
 
